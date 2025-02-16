17bf259b830f074a40bac39d84e24edb58d1ec13
Алексей Шатров <work@Noutbuk-Aleksej.local>
2025-02-16 22:54:22 +0300

    Changes in lifting structure

--- init.sql
+++ init.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS wallets (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     balance DECIMAL(10, 2),
     version BIGINT
-    );
+);
 
 INSERT INTO wallets (balance, version) VALUES (100.00, 1), (250.50, 1);
--- src/main/java/com/dev/walletrestapi/controllers/WalletController.java
+++ src/main/java/com/dev/walletrestapi/controllers/WalletController.java
@@ -6,7 +6,6 @@ import com.dev.walletrestapi.services.implementations.WalletServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
